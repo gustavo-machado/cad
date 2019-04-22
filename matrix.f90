272f5179e73500e4b63bc612f07c7eec19b04cc0
ianfreitas1 <ianfreitas_2005@hotmail.com>
2019-04-22 18:00:14 -0300

    Update matrix.f90
    Better comments and time counting

--- matrix.f90
+++ matrix.f90
@@ -1,66 +1,48 @@
-program matrixMultiplication
+program matrixVectorMultiplication
+
 implicit none
-integer, parameter :: n = 3 ! Parameter N defining size of matrices
-real(kind=8),allocatable::matriz(:,:),vetor(:),resultado1(:),resultado2(:)  ! Defining 3 matrices that will be allocatated in memory during executions
-integer(kind=4) :: i, j ! Iterators
-real :: start,finish ! Variables to make timing
-real(kind=8) :: r1,r2 
+integer, parameter :: n = 5000 ! Variavel que define o tamanho das matrizes
+real(kind=8),allocatable::matriz(:,:),vetor(:),resultado1(:),resultado2(:)  ! Definicao das 4 matrizes usadas no programa
+integer(kind=4) :: i, j ! Iteradores
+real :: start,finish ! Contagem de tempo
+real(kind=8) :: r1,r2 ! Numeros aleatorios
 
-allocate(matriz(n,n),vetor(n),resultado1(n),resultado2(n)) ! Alocating the matrices based on parameter n
+allocate(matriz(n,n),vetor(n),resultado1(n),resultado2(n)) ! Alocacao das matrizes
 
-! Filling up values to matrix 
+! Inicializando a matriz A com numeros aleatorios (Ax = B)
 do i = 1, n
- do j = 1, n    
+ do j = 1, n
     call random_number(r1)
     r1 = r1 * 100
     matriz(i, j) =  r1
   end do
 end do
 
-! Filling up values to vector 
+! Inicializando o vetor x com numeros aleatorios (Ax = B)
 do i = 1, n
     call random_number(r2)
     r2 = r2 * 100
     vetor(i) = r2
 end do
 
-!Tipo 1 de Multiplicacao
+! Tipo 1 de Multiplicacao (por linhas)
+call cpu_time(start)
 do j = 1, n
     do i = 1, n
-        resultado1(j) = resultado1(j) + matriz(j,i) * vetor(i) 
-    end do 
+        resultado1(j) = resultado1(j) + matriz(j,i) * vetor(i)
+    end do
 end do
+call cpu_time(finish)
+print '("Tempo (multiplicacao por linhas) = ",f6.3," segundos.")',finish-start
 
-!Tipo 2 de Multiplicacao
+! Tipo 2 de Multiplicacao (por colunas)
+call cpu_time(start)
 do i = 1, n
     do j = 1,n
-        resultado2(i) = resultado2(i) + matriz(j,i) * vetor(j)
+        resultado2(j) = resultado2(j) + matriz(j,i) * vetor(i)
     end do
 end do
+call cpu_time(finish)
+print '("Tempo (multiplicacao por colunas) = ",f6.3," segundos.")',finish-start
 
-do i = 1, n
-    do j = 1, n
-        print *, matriz(i,j)
-    end do
-end do
-print *,"Essa foi a matriz"
-
-do i = 1, n
-        print *, vetor(i)
-end do
-
-print *,"Essa foi o vetor"
-
-do i = 1, n
-        print *, resultado1(i)
-end do
-
-print *,"Essa foi o resultado1"
-
-do i = 1, n
-        print *, resultado2(i)
-end do
-
-print *,"Essa foi o resultado2"
-
-end program matrixMultiplication
+end program matrixVectorMultiplication
