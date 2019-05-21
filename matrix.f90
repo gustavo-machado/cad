6d7304e0cf30a61386ba383e28d802759b6a9ac1
ianfreitas1 <ianfreitas_2005@hotmail.com>
2019-05-20 22:19:14 -0300

    OpenMP fortran initial test

--- matrix.f90
+++ matrix.f90
@@ -1,11 +1,12 @@
 program matrixVectorMultiplication
 
 implicit none
-integer, parameter :: n = 5000 ! Variavel que define o tamanho das matrizes
+integer, parameter :: n = 13000 ! Variavel que define o tamanho das matrizes
 real(kind=8),allocatable::matriz(:,:),vetor(:),resultado1(:),resultado2(:)  ! Definicao das 4 matrizes usadas no programa
 integer(kind=4) :: i, j ! Iteradores
 real :: start,finish ! Contagem de tempo
 real(kind=8) :: r1,r2 ! Numeros aleatorios
+integer :: tid, omp_get_thread_num  
 
 allocate(matriz(n,n),vetor(n),resultado1(n),resultado2(n)) ! Alocacao das matrizes
 
@@ -19,17 +20,27 @@ do i = 1, n
 end do
 
 ! Inicializando o vetor x com numeros aleatorios (Ax = B)
+call cpu_time(start)
+!$ call omp_set_num_threads(2)
+!$omp parallel do schedule(static) &
+!$omp shared(vetor) &
+!$omp private(i, r2, tid)
 do i = 1, n
+    tid = omp_get_thread_num()
+    PRINT *, 'Hello World from thread = ', TID
     call random_number(r2)
     r2 = r2 * 100
     vetor(i) = r2
 end do
+!$omp end parallel do
+call cpu_time(finish)
+print '("Tempo (criacao do vetor) = ",f6.3," segundos.")',finish-start
 
 ! Tipo 1 de Multiplicacao (por linhas)
 call cpu_time(start)
 do j = 1, n
     do i = 1, n
-        resultado1(j) = resultado1(j) + matriz(j,i) * vetor(i)
+        resultado1(j) = resultado1(j) + matriz(j, i) * vetor(i)
     end do
 end do
 call cpu_time(finish)
