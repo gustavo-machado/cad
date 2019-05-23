12dd9f5a4b4493572ae6d4303a90c309920c80b7
gustavo-machado <totalgustavo0@gmail.com>
2019-05-22 21:36:00 -0300

    Inicio Matriz Otimizada

--- matrix2Flat.c
+++ matrix2Flat.c
@@ -4,27 +4,21 @@
 
 double **populaMatriz(int n);
 double *populaVetor(int n);
-double *multiplicacaoColunas(double **matriz, double *vetor, int n, double *resultado2);
-double *multiplicacaoLinhas(double **matriz, double *vetor, int n, double *resultado1);
+double *multiplicacaoLinhas(double **matriz, double *vetor, int n, double *resultado, double *flatA);
 
 
 int main(){
-    int n = 13000;
+    int n = 1000;
     double **matriz = populaMatriz(n);
     double *vetor = populaVetor(n);
-    double *resultado1 = malloc(n * sizeof(double));
-    double *resultado2 = malloc(n * sizeof(double));
+    double *resultado = malloc(n * sizeof(double));
+    double flatA[n*n];
+
     clock_t start = clock();
-    double *teste1 = multiplicacaoColunas(matriz,vetor,n,resultado2);
+    double *teste1 = multiplicacaoLinhas(matriz,vetor,n,resultado,flatA);
     clock_t end = clock();
     double tempo = (double)(end - start)/CLOCKS_PER_SEC;
     printf("O tempo da Multiplicacao mantendo coluna e %f\n",tempo);
-
-    clock_t start1 = clock();
-    double *teste2 = multiplicacaoLinhas(matriz,vetor,n, resultado1);
-    clock_t end1 = clock();
-    double tempo1 = (double)(end1 - start1)/ CLOCKS_PER_SEC;
-    printf("O tempo da Multiplicacao mantendo linha e %f\n",tempo1);
 
 }   
    
@@ -45,6 +39,16 @@ int main(){
         }
         return matriz;
     }
+
+    double *converteMatriz(int n, double **matriz){
+        double *flatA = malloc (n * n * sizeof(double));
+        #pragma omp parallel for
+        for(int i = 0;i < n; i++){
+            for (int j = 0;j < n; j++){
+                flatA[i * n + j] = matriz[i][j]; 
+            }
+        }
+    }
     /** Constroi o vetor por meio de um ponteiro 
      * Em seguida, ele alocao nesse ponteiro  um espaco de memoria de um double para cada posicao de memoria
      * E finalmente coloca um valor aleatorio na mesma**/
@@ -59,43 +63,20 @@ int main(){
     }
     /** Tipo de Multiplicacao 1 por linha
      * Segue a mesma ideia do fortran, somente tendo o ponteiro pro resultado sendo alocado um espaco de memoria do tamanho double para cada posicao**/
-    double *multiplicacaoLinhas(double **matriz, double *vetor, int n, double *resultado1){
-        
-        double *resultado_privado = malloc(n * sizeof(double));
+    double *multiplicacaoLinhas(double **matriz, double *vetor, int n, double *resultado, double *flatA){
         #pragma omp parallel for num_threads(40)
+        converteMatriz(n,matriz);
         for (int i=0; i < n; i++)
         {
+            int ioff = i * n;
+            int tot = 0; 
             for (int j=0; j < n; j++)
             {
-                resultado_privado[i] += vetor[j] * matriz[i][j];
+                for (int k=0; k < n; k++){
+                tot += flatA[ioff + k] * vetor[k];
+                }
+            resultado[i][j] = tot;
             }    
         }
-        #pragma omp parallel critical
-        for (int i=0; i < n; i++)
-        {
-            resultado1[i] += resultado_privado[i];
-        }
-        free(resultado_privado);
-        return resultado1;
-    }
-    /** Tipo de Multiplicacao 2 por COLUNA
-     * Segue a mesma ideia do fortran, somente tendo o ponteiro pro resultado sendo alocado um espaco de memoria do tamanho double para cada posicao**/
-    double *multiplicacaoColunas(double **matriz, double *vetor, int n, double *resultado2){
-        
-        double *resultado_privado = malloc(n * sizeof(double));
-        #pragma omp parallel for num_threads(40)
-        for (int j=0; j < n; j++)
-        {
-            for (int i=0; i < n; i++)
-            {
-                resultado_privado[i] += vetor[j] * matriz[i][j];
-            }    
-        }
-        #pragma omp parallel critical
-        for (int i=0; i < n; i++)
-        {
-            resultado2[i] += resultado_privado[i];
-        }
-        free(resultado_privado);
-        return resultado2;
+        return resultado;
     }
